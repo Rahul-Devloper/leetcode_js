2018e54974035f6a7a1fc413159561fdf5bd9db1
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-09-19 23:03:30 +0100

    added comments to ransomNote

--- ransomNote.js
+++ ransomNote.js
@@ -25,23 +25,38 @@
 
 
 /**
- * @param {string} ransomNote
- * @param {string} magazine
- * @return {boolean}
+ * Function to determine if a ransom note can be constructed from a magazine
+ * Each letter from the magazine can only be used once
+ * 
+ * @param {string} ransomNote - The string we want to construct
+ * @param {string} magazine - The source string we can use letters from
+ * @return {boolean} - Returns true if ransomNote can be constructed from magazine
  */
 var canConstruct = function(ransomNote, magazine) {
+    // Create a Map to store character frequencies from magazine
     const map = new Map()
 
+    // First pass: Count frequency of each character in magazine
     for (let char of magazine){
+        // If char exists, increment its count, if not set it to 1
         map.set(char, (map.get(char) || 0) + 1)
     }
 
+    // Second pass: Check if we have enough characters for the ransom note
     for (let char of ransomNote){
+        // If character doesn't exist in map or its count is 0
+        // we can't construct the note
         if(!map.has(char) || map.get(char) === 0) return false
 
+        // Decrease the frequency count as we use this character
         map.set(char, (map.get(char) - 1))
 
+        // Note: This commented line would make the function return true
+        // too early, which would be incorrect
         // if(map.get(char) === 0) return true
     }
+    
+    // If we've made it through the entire ransomNote without returning false,
+    // we have all the characters we need
     return true
 };
