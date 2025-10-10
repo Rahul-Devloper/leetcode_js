638655a38026646c36393347dc6a6a2db4e31082
Rahul-Devloper <rahulramesh26@yahoo.com>
2025-10-10 22:22:48 +0100

    duplicate

--- redoSet/hashmap/containsDuplicate.js
+++ redoSet/hashmap/containsDuplicate.js
@@ -4,13 +4,14 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function (nums, k) {
-    const map = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i])) {
-            if( i - map.get(nums[i]) <= k) return true
-        }
-        
-        map.set(nums[i], i)
+  const map = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      if (i - map.get(nums[i]) <= k) return true
     }
-    return false
+
+    map.set(nums[i], i)
+  }
+  return false
 }
